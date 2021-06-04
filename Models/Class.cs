e2dd5ffac358d4e4619cf2392b3327a82a214db8
KennethM <mendenhall_kenneth1@columbusstate.edu>
2021-06-04 13:17:12 -0400

    Add project files.

---
+++ Models/Class.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string StuFirstName { get; set; }
+        public string StuLastName { get; set; }
+        public int StuAge { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime StuEnrollDate { get; set; }
+
+    }
+
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public int CourseCapacity { get; set; }
+        public int CourseCountEnrollment { get; set; }
+
+    }
+
+    public class Department
+    {
+        public int DepID { get; set; }
+        public string DepName { get; set; }
+        public int DepChair { get; set; }
+    }
+}
---
+++ Pages/Student/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Pages.Student
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly WebApplication1.Data.WebApplication1Context _context;
+
+        public DetailsModel(WebApplication1.Data.WebApplication1Context context)
+        {
+            _context = context;
+        }
+
+        public Student Student { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Student = await _context.Student.FirstOrDefaultAsync(m => m.StudentID == id);
+
+            if (Student == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
